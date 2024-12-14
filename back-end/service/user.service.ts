58d073966e6e99132d61a5d4e864e66cc2f0985e
amelielammensUCLL <amelie.lammens@student.ucll.be>
2024-12-14 12:42:07 +0100

    userservice changed

--- back-end/service/user.service.ts
+++ back-end/service/user.service.ts
@@ -2,7 +2,14 @@ import { User } from "../model/user";
 import userDb from "../repository/user.db"
 
 const getAllUsers = async (): Promise<User[]> => {
-    return userDb.getAllUsers();
+
+    const users = await userDb.getAllUsers();
+    if (!users || users.length === 0) {
+        throw new Error("No users found");
+    }
+    return users;
+    
+    // return userDb.getAllUsers();
 };
 
 const getUserById = async (id: number): Promise<User | null> => {
@@ -15,7 +22,12 @@ const getUserById = async (id: number): Promise<User | null> => {
 };
 
 const getUserByEmail = async (email: string): Promise<User | null> => {
-    return userDb.getUserByEmail(email);
+    const user = await userDb.getUserByEmail(email);
+    if (!user) {
+        throw new Error("User doesn't exist");
+    }
+    return user;
+    // return userDb.getUserByEmail(email);
 };
 
 export default {
