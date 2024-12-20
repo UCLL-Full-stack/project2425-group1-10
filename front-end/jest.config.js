7f95970a21e77e67427efd0abac5d05c68a06d09
amelielammensUCLL <amelie.lammens@student.ucll.be>
2024-12-20 14:32:57 +0100

    small frontend test

---
+++ front-end/jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+/* @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+  testEnvironment: 'jsdom',
+transform: {
+'\\.[jt]sx?$': 'esbuild-jest',
+},
+};
